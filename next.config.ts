a6e44be97c5aa3dbbcfc14d6139e3d1ea8122ef5
KaichenQu <qu.ka@northeastern.edu>
2025-03-04 21:12:41 -0800

    config redirecting

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig: NextConfig = {
     domains: ["fonts.googleapis.com"],
   },
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "www.kelsonqu.com",
+        destination: "https://kelsonqu.com",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
